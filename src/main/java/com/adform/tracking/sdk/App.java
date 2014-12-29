0a14b4acaa098c667faba11290fbc05f07330482
mariusm <marius.m@ito.lt>
2014-12-29 09:44:46 +0200

    initinal commit

---
+++ src/main/java/com/adform/tracking/sdk/App.java
@@ -0,0 +1,13 @@
+package com.adform.tracking.sdk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
